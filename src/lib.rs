e5631c7c9f24aad915172a4e54e462e631a3a4a2
robinohs <royalstudio@outlook.de>
2023-02-11 17:54:48 +0100

    += Assert tokenstreams are equal with pretty_assertions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,147 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+    string::FromUtf8Error,
+};
+
+use pretty_assertions::assert_eq;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum RustfmtError {
+    #[error("Could not create child process for rustfmt.")]
+    Io(#[from] std::io::Error),
+    #[error("Could not convert output bytes to UTF-8.")]
+    Utf8(#[from] FromUtf8Error),
+    #[error("Could not read from rustfmt child process.")]
+    Output,
+    #[error("Could not access stdin of rustfmt child process.")]
+    Stdin,
+    #[error("Input is no valid rust code.\n{0}")]
+    InvalidRust(String),
+}
+
+pub fn compare_tokenstreams(first_tokenstream: impl ToString, second_tokenstream: impl ToString) {
+    let first_formatted = match apply_rustfmt(first_tokenstream) {
+        Ok(tokens) => tokens,
+        Err(e) => panic!("{}", e),
+    };
+    let second_formatted = match apply_rustfmt(second_tokenstream) {
+        Ok(tokens) => tokens,
+        Err(e) => panic!("{}", e),
+    };
+    assert_eq!(first_formatted, second_formatted)
+}
+
+fn apply_rustfmt(tokens: impl ToString) -> Result<String, RustfmtError> {
+    let mut process = Command::new("rustfmt")
+        .arg("--")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+
+    let stdin = match process.stdin.as_mut() {
+        Some(stdin) => stdin,
+        None => return Err(RustfmtError::Stdin),
+    };
+
+    stdin.write_all(tokens.to_string().as_bytes())?;
+
+    let output = process
+        .wait_with_output()
+        .map_err(|_| RustfmtError::Output)?;
+
+    if output.status.success() {
+        let fmt_tokens = String::from_utf8(output.stdout)?;
+        Ok(fmt_tokens)
+    } else {
+        let err = String::from_utf8(output.stderr)?;
+        Err(RustfmtError::InvalidRust(err))
+    }
+}
+
+#[macro_export]
+macro_rules! assert_tokenstreams_eq {
+    ($left:expr, $right:expr) => {{
+        $crate::compare_tokenstreams($left, $right);
+    }};
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use quote::quote;
+
+    use crate::assert_tokenstreams_eq;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_compare_tokenstreams_equal() {
+        let first = quote! {
+            fn test(a: String, b: String) {
+                return a;
+            }
+        };
+        let second = quote! {
+            fn test(a: String, b: String) {
+                return a;
+            }
+        };
+        assert_tokenstreams_eq!(first, second);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_compare_tokenstreams_unequal() {
+        let first = quote! {
+            fn test(a: String, b: String) {
+                let test = 2;
+                return a;
+            }
+        };
+        let second = quote! {
+            fn test2(a: String, b: String) {
+                let test = 2;
+                return b;
+            }
+        };
+        assert_tokenstreams_eq!(first, second);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_compare_tokenstreams_invalid_rust_code() {
+        let first = quote! {
+            async fn test(a: String, b: String) {
+                let test = async {
+                    a + b
+                }
+                return test;
+            }
+        };
+        let second = quote! {
+            async fn test(a: String, b: String) {
+                return a;
+            }
+        };
+        assert_tokenstreams_eq!(first, second);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_compare_tokenstreams_unequal_async() {
+        let first = quote! {
+            async fn test(a: String, b: String) {
+                let test = async {
+                    a + b
+                }.await
+                return test;
+            }
+        };
+        let second = quote! {
+            async fn test(a: String, b: String) {
+                return a;
+            }
+        };
+        assert_tokenstreams_eq!(first, second);
     }
 }
